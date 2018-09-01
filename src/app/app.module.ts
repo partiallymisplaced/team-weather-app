5619fc444fd255c7865c8f611e6bf8dbbbb5632d
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-01 16:57:42 -0700

    Adds header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CurrentWeatherComponent
+    CurrentWeatherComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
