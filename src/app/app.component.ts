e48c00e685b8546f3a58ff2af1a0c53ed607792f
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-04 22:27:52 -0700

    Adds function to apply night background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  // title = 'Weather-o';
+
+export class AppComponent implements OnInit {
+   applyNightBackground() {
+    const currentHour = new Date().getHours();
+    
+    let background = document.querySelector('body');
+      if (currentHour <= 7 || currentHour >= 20) {
+        background.classList.add('night-color');
+      }  
+  }
+
+  ngOnInit() {
+    this.applyNightBackground();
+  }
 }
